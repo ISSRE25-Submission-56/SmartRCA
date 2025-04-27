8ea0d58934ffab7d3d26cc7f40d6c5358a9f33ba
ccuitianyu <cuitianyu@mail.nankai.edu.cn>
2025-04-27 23:43:18 +0800

    Create knowledge_graph.py

---
+++ knowledge_graph.py
@@ -0,0 +1,26 @@
+import networkx as nx
+
+def build_knowledge_graph(entities, clusters):
+    """
+    基于实体和聚类结果构建知识图谱。
+    
+    Args:
+    entities (list): 抽取的实体列表
+    clusters (list): 聚类标签
+    
+    Returns:
+    NetworkX Graph: 知识图谱，图的节点是实体，边是基于实体的相似度连接
+    """
+    G = nx.Graph()
+    
+    # 添加实体节点，并将聚类信息作为节点属性
+    for idx, entity in enumerate(entities):
+        G.add_node(entity['word'], cluster=clusters[idx])
+    
+    # 这里可以根据一些规则或相似度为实体之间添加边
+    # 假设相邻的实体在相同聚类中会有边
+    for i in range(len(entities) - 1):
+        if clusters[i] == clusters[i + 1]:
+            G.add_edge(entities[i]['word'], entities[i + 1]['word'])
+    
+    return G
